bc2744139e9421a618f49165810431fb423bd890
hker9527 <owo@uwu.cc>
2021-10-27 02:22:51 +0800

    Update permission

--- src/modules/utils/invite.ts
+++ src/modules/utils/invite.ts
@@ -7,6 +7,6 @@ export const module: Module = {
 		"id": "client.user.id"
 	},
 	action: async (obj: ModuleActionArgument) => {
-		return obj.message.reply("<https://discordapp.com/oauth2/authorize?&client_id=" + obj.eval!.id + "&scope=bot&permissions=252992>");
+		return obj.message.reply(`<https://discordapp.com/oauth2/authorize?&client_id=${obj.eval!.id}&scope=bot&permissions=523328>`);
 	}
 };
