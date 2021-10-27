61c4db2ff4cc6ff4174884cd4649cb905caf4536
hker9527 <owo@uwu.cc>
2021-10-27 22:45:18 +0800

    Implement osu link

--- src/modules/osu/osu_link.ts
+++ src/modules/osu/osu_link.ts
@@ -1,11 +1,41 @@
 // A heartful sorry for users...
 import { getString } from "@app/i18n";
-import { Module, ModuleActionArgument } from '@type/Module';
+import { Singleton } from "@app/Singleton";
+import { ArgumentRequirement, Module, ModuleActionArgument } from '@type/Module';
+import { assert } from "console";
 
 export const module: Module = {
 	trigger: ["link"],
 	event: "messageCreate",
+	argv: {
+		id: [ArgumentRequirement.Required, ArgumentRequirement.Concat],
+	},
 	action: async (obj: ModuleActionArgument) => {
-		return await obj.message.reply(getString("migrate", obj.message.getLocale()));
+		const osuId = Singleton.db!.data!.osuLink[obj.message.author.id];
+
+		if (osuId) {
+			return await obj.message.reply(getString("osu.link.exists", obj.message.getLocale()));
+		}
+
+		try {
+			const apiResponse = await Singleton.osuClient!.users.get(
+				obj.argv!.id
+			);
+
+			assert(apiResponse.user_id);
+
+			Singleton.db!.data!.osuLink[obj.message.author.id] = apiResponse.user_id;
+
+			return await obj.message.reply(
+				getString("osu.link.linked", obj.message.getLocale(), {
+					discord_id: obj.message.author.toString(),
+					osu_id: apiResponse.username
+				})
+			);
+		} catch (e) {
+			return await obj.message.reply(
+				getString("osu.error", obj.message.getLocale(), { id: obj.argv!.id })
+			);
+		}
 	}
 };
