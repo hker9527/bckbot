d1863185fa1fc964297093a76037e04f573be761
hker9527 <owo@uwu.cc>
2022-01-08 00:32:07 +0800

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: [
+        "typescript",
+		"@typescript-eslint"
+    ],
+	extends: [
+		"eslint-config-alloy/typescript"
+	],
+    rules: {
+		"@typescript-eslint/consistent-type-assertions": "off",
+		"@typescript-eslint/member-ordering": "off",
+		"no-case-declarations": "off",
+		"no-undef": "off",
+		"no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": "error",
+		"eqeqeq": "error",
+		"quotes": ["error", "double"]
+    }
+}
