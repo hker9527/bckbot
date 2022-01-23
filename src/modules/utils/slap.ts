4672ae56db5af107438fa1825a5d2f05231f1fbd
hker9527 <owo@uwu.cc>
2022-01-24 03:48:33 +0800

    Reform to the new API

--- src/modules/utils/slap.ts
+++ src/modules/utils/slap.ts
@@ -1,4 +1,3 @@
-import { getString } from "@app/i18n";
 import { arr2obj, random, urandom } from "@app/utils";
 import { SlashCommand } from "@type/SlashCommand";
 import emoji from "node-emoji";
@@ -21,22 +20,25 @@ export const module: SlashCommand = {
 		}
 	],
 	onCommand: async (interaction) => {
-		return getString("slap.slap", interaction.getLocale(), {
-			slapper: interaction.member!.toString(),
-			victim: interaction.options.getString("victim", true),
-			// TODO: Fix emoji that cannot display (message.react?)
-			tool: (interaction.options.getString("tool") ?? emoji.random().emoji),
-			damage: urandom(
-				arr2obj(
-					[
-						random(50, 100),
-						random(100, 300),
-						random(300, 600),
-						random(600, 1000)
-					],
-					[0.1, 0.6, 0.2, 0.1]
+		return {
+			key: "slap.slap",
+			data: {
+				slapper: interaction.member!.toString(),
+				victim: interaction.options.getString("victim", true),
+				// TODO: Fix emoji that cannot display (message.react?)
+				tool: (interaction.options.getString("tool") ?? emoji.random().emoji),
+				damage: urandom(
+					arr2obj(
+						[
+							random(50, 100),
+							random(100, 300),
+							random(300, 600),
+							random(600, 1000)
+						],
+						[0.1, 0.6, 0.2, 0.1]
+					)
 				)
-			)
-		});
+			}
+		};
 	}
 };
