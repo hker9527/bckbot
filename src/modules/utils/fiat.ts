baad014b28af5d023b053c7455cccc1be27c17f6
hker9527 <owo@uwu.cc>
2022-04-29 01:26:21 +0800

    Fix fiat

--- src/modules/utils/fiat.ts
+++ src/modules/utils/fiat.ts
@@ -1,26 +1,48 @@
-import { Singleton } from "@app/Singleton";
 import { enumStringKeys, req2json, round } from "@app/utils";
-import { Currencies } from "@type/Database";
+import { PrismaClient } from "@prisma/client";
+import { Currency } from "@type/Currency";
 import { SlashCommand } from "@type/SlashCommand";
 import assert from "assert";
+import { z } from "zod";
 
-let data = Singleton.db.data!.currency;
+const prisma = new PrismaClient();
 
 const worker = async () => {
-	let response;
 	try {
-		for (let i in data) {
-			const c1 = i as keyof typeof Currencies;
-			for (let j in data.data[c1]) {
-				const c2 = j as keyof typeof Currencies;
-				response = await req2json(`https://free.currconv.com/api/v7/convert?q=${i}_${j},${j}_${i}&compact=ultra&apiKey=${process.env.currency}`);
-				assert(!isNaN(response[`${i}_${j}`]) && !isNaN(response[`${j}_${i}`]));
-				data.data[c1][c2] = response[`${i}_${j}`];
-				data.data[c2][c1] = response[`${j}_${i}`];
+		const updateInfo = await prisma.currencyUpdateInfo.create({ data: {} });
+		const currencies = enumStringKeys(Currency);
+
+		const requestPairs = [
+			[...new Array(currencies.length - 1)].map((_, i) =>
+				[...new Array(currencies.length - i - 1)].map((_, j) => [currencies[i], currencies[j + i + 1]]))
+		].flat().flat();
+
+		for (let i = 0; i < Math.floor(requestPairs.length / 2); i++) {
+			const pairs = requestPairs.slice(i * 2, (i + 1) * 2);
+			const response = await req2json(`https://free.currconv.com/api/v7/convert?q=${pairs.map(p => p.join("_")).join(",")}&compact=ultra&apiKey=${process.env.currency}`);
+			const Z = z.object({
+				[pairs[0].join("_")]: z.number(),
+				[pairs[1].join("_")]: z.number()
+			});
+			const data = Z.safeParse(response);
+			assert(data.success);
+
+			for (const pair of pairs) {
+				await prisma.currencyRecord.create({
+					data: {
+						src: pair[0],
+						dst: pair[1],
+						value: data.data[`${pair[0]}_${pair[1]}`],
+						updateInfo: {
+							connect: {
+								id: updateInfo.id
+							}
+						}
+					}
+				});
 			}
 		}
 
-		data.lastUpdate = new Date();
 		return true;
 	} catch (e) {
 		return false;
@@ -32,35 +54,69 @@ setInterval(worker, 3600 * 1000);
 
 export const module: SlashCommand = {
 	name: "currency",
-	description: "Spot currency conversion",
+	description: {
+		key: "fiat.description"
+	},
 	options: [{
 		name: "source",
-		description: "From what currency",
+		description: {
+			key: "fiat.sourceDescription"
+		},
 		type: "STRING",
-		choices: enumStringKeys(Currencies).map(currency => ({
+		choices: enumStringKeys(Currency).map(currency => ({
 			name: currency,
 			value: currency
 		}))
 	}, {
 		name: "amount",
-		description: "How much",
+		description: {
+			key: "fiat.amountDescription"
+		},
 		type: "NUMBER",
 		min_value: 1,
 		optional: true
 	}, {
 		name: "target",
-		description: "To what currency, defaults to all",
+		description: {
+			key: "fiat.targetDescription"
+		},
 		type: "STRING",
-		choices: enumStringKeys(Currencies).map(currency => ({
+		choices: enumStringKeys(Currency).map(currency => ({
 			name: currency,
 			value: currency
 		})),
 		optional: true
 	}],
 	onCommand: async (interaction) => {
-		const source = interaction.options.getString("source", true) as keyof typeof Currencies;
+		const source = interaction.options.getString("source", true) as keyof typeof Currency;
 		const amount = interaction.options.getNumber("amount") ?? 1;
-		const target = interaction.options.getString("target") as keyof typeof Currencies ?? null;
+		const target = interaction.options.getString("target") as keyof typeof Currency ?? null;
+
+		const currencies = enumStringKeys(Currency);
+
+		const records = await prisma.currencyRecord.findMany({
+			where: {
+				OR: [
+					{
+						src: source
+					},
+					{ 
+						dst: source
+					}
+				]
+			},
+			orderBy: {
+				updateInfo: {
+					time: "desc"
+				}
+			},
+			include: {
+				updateInfo: true
+			},
+			take: currencies.length - 1
+		});
+
+		assert(records.length === currencies.length - 1);
 
 		return {
 			embeds: [{
@@ -70,16 +126,23 @@ export const module: SlashCommand = {
 						name: source,
 						value: amount.toString()
 					},
-					...enumStringKeys(Currencies).filter(currency => currency !== source && ((target !== null && currency === target) || target === null)).map(currency => ({
-						name: currency,
-						value: round(data.data[source][currency as keyof typeof Currencies] * amount, 2).toString(),
-						inline: true
-					}))
+					...records
+						.filter(record => target === null ? true : record.dst === target || record.src === target)
+						.map(record => {
+							// Assumption: src -> dst = 1 / dst -> src
+
+							const inverseFlag = record.dst === source;
+							return {
+								name: inverseFlag ? record.src : record.dst,
+								value: `${round((inverseFlag ? 1 / record.value : record.value) * amount, 3)}`,
+								inline: true
+							}
+						})
 				],
 				footer: {
 					text: "Updated at"
 				},
-				timestamp: data.lastUpdate
+				timestamp: records[0].updateInfo.time
 			}]
 		};
 	}
