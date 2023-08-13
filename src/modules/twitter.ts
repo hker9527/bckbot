8e6c8bf0ef5b3b315b0dc64443318e16f2364054
hker9527 <owo@uwu.cc>
2023-08-13 17:23:00 +0800

    twitter: Remove permission check

--- src/modules/twitter.ts
+++ src/modules/twitter.ts
@@ -1,6 +1,6 @@
 import { StealthModule } from "@type/StealthModule";
 import { ZAPIVXTwitter } from "@type/api/VXTwitter";
-import { APIEmbed, PermissionsBitField } from "discord.js";
+import { APIEmbed } from "discord.js";
 
 export const twitter: StealthModule = {
 	name: "twitter",
@@ -19,41 +19,37 @@ export const twitter: StealthModule = {
 					return false;
 				}
 
-				// Check if message channel can send message permission
-				const member = obj.message.guild!.members.cache.get(obj.message.client.user!.id)!;
-				if (member.permissions.has(PermissionsBitField.Flags.SendMessages)) {
-					try {
-						await obj.message.suppressEmbeds();
-					} catch (e) {}
+				try {
+					await obj.message.suppressEmbeds();
+				} catch (e) {}
 
-					return {
-						type: "send",
-						result: {
-							embeds: ([
-								{
-									author: {
-										name: `${json.user_name} (@${json.user_screen_name})`,
-										icon_url: "https://cdn-icons-png.flaticon.com/512/179/179342.png",
-										url: `https://twitter.com/${json.user_screen_name}`
-									},
-									color: 0x1DA1F2,
-									// Remove https://t.co/... links
-									description: json.text.replace(/https:\/\/t\.co\/\w+/g, ""),
-									timestamp: new Date(json.date_epoch * 1000).toISOString(),
-									image: {
-										url: images[0].url
-									},
-									url: json.tweetURL
-								}
-							] as APIEmbed[]).concat(images.splice(1).map((image) => ({
+				return {
+					type: "send",
+					result: {
+						embeds: ([
+							{
+								author: {
+									name: `${json.user_name} (@${json.user_screen_name})`,
+									icon_url: "https://cdn-icons-png.flaticon.com/512/179/179342.png",
+									url: `https://twitter.com/${json.user_screen_name}`
+								},
+								color: 0x1DA1F2,
+								// Remove https://t.co/... links
+								description: json.text.replace(/https:\/\/t\.co\/\w+/g, ""),
+								timestamp: new Date(json.date_epoch * 1000).toISOString(),
 								image: {
-									url: image.url
+									url: images[0].url
 								},
 								url: json.tweetURL
-							}))) 
-						}
-					};
-				}
+							}
+						] as APIEmbed[]).concat(images.splice(1).map((image) => ({
+							image: {
+								url: image.url
+							},
+							url: json.tweetURL
+						}))) 
+					}
+				};
 			}
 			return false;
 		} catch (e) {
