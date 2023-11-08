b91298fcbc2c293cda25a36bea9679771f40f6a6
hker9527 <owo@uwu.cc>
2023-11-08 22:29:40 +0800

    twitter: Use FXTwitter as provider, enable multiple source detection

--- src/modules/twitter.ts
+++ src/modules/twitter.ts
@@ -1,76 +1,158 @@
-import { sleep } from "@app/utils";
+import { debug } from "@app/Reporting";
+import { num2str, sleep } from "@app/utils";
+import { LAPIEmbed } from "@localizer/data/APIEmbed";
+import { LActionRowComponentData, LActionRowData } from "@localizer/data/ActionRowData";
 import { StealthModule } from "@type/StealthModule";
-import { ZAPIVXTwitter } from "@type/api/VXTwitter";
-import { APIEmbed } from "discord.js";
+import { APIFXTwitter, ZAPIFXTwitter } from "@type/api/FXTwitter";
+import { find } from "linkifyjs";
 
 export const twitter: StealthModule = {
 	name: "twitter",
 	event: "messageCreate",
-	pattern: /https?:\/\/(?:www\.)?(twitter|x)\.com\/(?:#!\/)?(\w+)\/status\/(\d+)/,
 	action: async (obj) => {
-		// Wait for embed to populate
-		await sleep(5000);
-		
-		// Fetch newest version message (Reload embeds)
-		obj.message = await obj.message.channel.messages.fetch(obj.message.id);
+		const url = find(obj.message.content)
+			.filter(result => result.type === "url")
+			.map(result => new URL(result.href))
+			.filter(url => [
+					"vxtwitter.com",
+					"fixvx.com",
+					"fxtwitter.com",
+					"fixupx.com",
+					"twittpr.com",
+					"twitter.com",
+					"x.com"
+				].some(domain => url.hostname.endsWith(domain))
+			)[0];
 
-		// Check if original message has images in embed (Normal behavior)
-		if (obj.message.embeds.length > 0 && obj.message.embeds[0].image) {
+		if (!url) {
 			return false;
 		}
 
-		const statusId = obj.matches!.pop();
-		const author = obj.matches!.pop();
+		const vanilla = /^(www\.)?(twitter|x)\.com$/.test(url.hostname);
 
-		try {
-			const response = await (await fetch(`https://api.vxtwitter.com/${author}/status/${statusId}`)).text();
-			const json = JSON.parse(response);
-			if (ZAPIVXTwitter.check(json)) {
-				const images = json.media_extended.filter((media) => media.type === "image");
+		// Fetch response in background
+		const [hasEmbed, json] = await Promise.all([
+			new Promise<boolean>(async (resolve) => {
+				// Wait for embed to populate
+				// await sleep(2000);
 
-				if (images.length === 0) {
-					return false;
-				}
+				// Fetch newest version message (Reload embeds)
+				obj.message = await obj.message.channel.messages.fetch(obj.message.id);
 
+				resolve(obj.message.embeds.length > 0);
+			}),
+			new Promise<APIFXTwitter["tweet"] | null>(async (resolve) => {
+				const [, author, , statusId] = url.pathname.split("/");
+
+				let response: string | null = null;
 				try {
-					await obj.message.suppressEmbeds();
-				} catch (e) {}
-
-				return {
-					type: "reply",
-					result: {
-						embeds: ([
-							{
-								author: {
-									name: `${json.user_name} (@${json.user_screen_name})`,
-									icon_url: `https://unavatar.io/twitter/${json.user_screen_name}`,
-									url: `https://twitter.com/${json.user_screen_name}`
-								},
-								color: 0x1DA1F2,
-								// Remove https://t.co/... links
-								description: json.text.replace(/https:\/\/t\.co\/\w+/g, ""),
-								footer: {
-									text: `❤️ ${json.likes} 🔁 ${json.retweets} 🗨️ ${json.replies}`,
-									icon_url: "https://cdn-icons-png.flaticon.com/512/179/179342.png"
-								},
-								timestamp: new Date(json.date_epoch * 1000).toISOString(),
-								image: {
-									url: images[0].url
-								},
-								url: json.tweetURL
-							}
-						] as APIEmbed[]).concat(images.splice(1).map((image) => ({
-							image: {
-								url: image.url
-							},
-							url: json.tweetURL
-						}))) 
+					response = await (await fetch(`https://api.fxtwitter.com/${author}/status/${statusId}`)).text();
+					const json = JSON.parse(response);
+					if (ZAPIFXTwitter.check(json, false)) {
+						resolve(json.tweet);
 					}
-				};
-			};
-			return false;
-		} catch (e) {
+				} catch (e) {
+					debug("twitter", `Failed to parse response ${response}: ${e}`);
+					resolve(null);
+				}
+			})
+		]);
+
+		if (vanilla && hasEmbed || !json) {
 			return false;
 		}
+
+		const images = json.media?.photos ?? [];
+		const videos = json.media?.videos ?? [];
+
+		const imageUrls = [
+			...images.map((image) => image.url),
+			...videos.map((video) => video.thumbnail_url)
+		];
+
+		if (!vanilla && (
+			images.length < 2 // The fix-up sites jam all the images together
+			|| videos.length > 0 // No video support
+		)) {
+			return false;
+		}
+
+		let embeds: LAPIEmbed[] = [
+			{
+				author: {
+					name: `${json.author.name} (@${json.author.screen_name})`,
+					iconURL: json.author.avatar_url,
+					url: json.author.url
+				},
+				color: 0x1DA1F2,
+				description: {
+					key: "twitter.description",
+					data: {
+						description: json.text + (json.quote ? `\n>>> **${json.quote.author.name} (@${json.quote.author.screen_name})**\n${json.quote.text}` : ""),
+						url: json.url
+					}
+				},
+				footer: {
+					text: `❤️ ${num2str(json.likes)} 🔁 ${num2str(json.retweets)} 🗨️ ${num2str(json.replies)}${json.views ? " 👀 " + num2str(json.views) : ""}`,
+					iconURL: "https://cdn-icons-png.flaticon.com/512/179/179342.png"
+				},
+				timestamp: new Date(json.created_timestamp * 1000).toISOString(),
+				url: json.url
+			}
+		];
+
+		if (imageUrls.length > 0) {
+			embeds[0].image = {
+				url: imageUrls[0]
+			};
+
+			embeds = embeds.concat(imageUrls.splice(1).map((imageUrl) => ({
+				image: {
+					url: imageUrl
+				},
+				url: json.url
+			})));
+		}
+
+		if (videos.length > 0) {
+			embeds[0].fields = [
+				{
+					name: {
+						key: "twitter.unsupportedMediaHeader"
+					},
+					value: {
+						key: "twitter.unsupportedMediaContent",
+						data: {
+							url: json.url
+						}
+					}
+				}
+			];
+		}
+		try {
+			await obj.message.suppressEmbeds();
+		} catch (e) { }
+
+		return {
+			type: "reply",
+			result: {
+				embeds,
+				components: [
+					{
+						type: "ActionRow",
+						components: [
+							{
+								type: "Button",
+								label: {
+									key: "originalTweetButton"
+								},
+								style: "Link",
+								url: json.url
+							}
+						]
+					}
+				] as LActionRowData[]
+			}
+		};
 	}
 };
---
+++ src/types/api/FXTwitter.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { Zod } from "@type/Zod";
+
+const BaseTweetSchema = z.object({
+    url: z.string(),
+    text: z.string(),
+    author: z.object({
+        name: z.string(),
+        screen_name: z.string(),
+        avatar_url: z.string(),
+        url: z.string()
+    }),
+    replies: z.number(),
+    retweets: z.number(),
+    likes: z.number(),
+    created_timestamp: z.number(),
+    possibly_sensitive: z.boolean().optional(),
+    views: z.number().nullable(),
+    replying_to: z.string().nullable(),
+    replying_to_status: z.string().nullable(),
+    color: z.null(),
+    media: z.object({
+        photos: z.array(z.object({
+            url: z.string()
+        })).optional(),
+        videos: z.array(z.object({
+            thumbnail_url: z.string()
+        })).optional()
+    }).optional()
+});
+
+const TweetSchema = BaseTweetSchema.extend({
+    quote: BaseTweetSchema.optional()
+});
+
+const FxTwitterSchema = z.object({
+    code: z.number(),
+    message: z.string(),
+    tweet: TweetSchema
+});
+
+export const ZAPIFXTwitter = new Zod(FxTwitterSchema);
+
+export type APIFXTwitter = z.infer<typeof FxTwitterSchema>;
