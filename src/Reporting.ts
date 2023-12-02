45fa2339038e5a7a846597168a97914f31346b50
hker9527 <owo@uwu.cc>
2023-12-02 22:05:25 +0800

    Add line number to logging

--- src/Reporting.ts
+++ src/Reporting.ts
@@ -1,13 +1,22 @@
 const flag = process.env.NODE_ENV !== "production";
 
+const getPrefix = () => {
+	const error = new Error();
+	const stack = error.stack!.split("\n");
+	const path = stack[3].trim().split(" ").pop();
+
+	// Replace base path
+	return `[${path!.replace(process.cwd() + "/", "").replace(/\(|\)/g, "")}]`;
+};
+
 export const report = (string: string) => {
-	console.log(`${string}`);
+	console.log(`${getPrefix()} ${string}`);
 };
 
 export const debug = (tag: string, e: unknown) => {
-	if (flag) console.debug(`[${tag}] ${e}`);
+	if (flag) console.debug(`${getPrefix()} [${tag}] ${e}`);
 };
 
 export const error = (tag: string, e: unknown) => {
-	if (flag) console.error(`[${tag}] ${e}`);
+	if (flag) console.error(`${getPrefix()} [${tag}] ${e}`);
 };
