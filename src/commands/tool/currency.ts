ffcb430b4f5f2bdded4117b02bcd10b65bb56002
hker9527 <owo@uwu.cc>
2024-03-20 14:50:32 +0800

    currency: Drop DB implementation

--- src/commands/tool/currency.ts
+++ src/commands/tool/currency.ts
@@ -2,73 +2,98 @@ import { arr2obj, round } from "@app/utils";
 import { SlashApplicationCommand } from "@class/ApplicationCommand";
 import type { LApplicationCommandOptionData } from "@class/ApplicationCommandOptionData";
 import type { LInteractionReplyOptions } from "@localizer/InteractionReplyOptions";
-import { PrismaClient } from "@prisma/client";
 import { Zod } from "@type/Zod";
 import assert from "assert-ts";
 import type { ChatInputCommandInteraction } from "discord.js";
 import { z } from "zod";
+import { Logger } from "tslog";
+
+const logger = new Logger({
+	name: "currency",
+	minLevel: process.env.DEV === "true" ? 0 : 3
+});
 
 const currencies = [
 	"TWD", "HKD", "JPY", "USD", "EUR"
 ];
 
-const prisma = new PrismaClient();
+const lastUpdated = new Date(0);
+let quotes: Record<string, number> = {};
+
+const getQuote = (source: string, target: string, amount: number) => {
+	if (source === target) {
+		return 1;
+	}
+
+	return round(quotes[source + target] * amount, 3);
+};
 
 const worker = async () => {
+	const sublogger = logger.getSubLogger({
+		name: "worker"
+	});
+
 	try {
-		// Check last updated
-		const lastUpdateInfo = await prisma.currencyUpdateInfo.findFirst({
-			orderBy: {
-				time: "desc"
-			}
-		});
-		if (lastUpdateInfo !== null) {
-			const diff = Date.now() - lastUpdateInfo.time.getTime();
-			if (diff < 3600 * 1000) {
-				return true;
-			}
+		if (process.env.DEV === "true") {
+			logger.debug("Using mock data");
+			quotes = {
+				"TWDHKD": 0.25,
+				"TWDJPY": 3.5,
+				"TWDUSD": 0.035,
+				"TWDEUR": 0.03,
+				"HKDTWD": 4,
+				"HKDJPY": 14,
+				"HKDUSD": 0.14,
+				"HKDEUR": 0.12,
+				"JPYTWD": 0.285,
+				"JPYHKD": 0.071,
+				"JPYUSD": 0.009,
+				"JPYEUR": 0.008,
+				"USDTWD": 28.5,
+				"USDHKD": 7,
+				"USDJPY": 110,
+				"USDEUR": 0.85,
+				"EURTWD": 33.5,
+				"EURHKD": 8.5,
+				"EURJPY": 130,
+				"EURUSD": 1.18
+			};
+
+			return true;
 		}
 
-		const updateInfo = await prisma.currencyUpdateInfo.create({ data: {} });
+		quotes = {};
 
 		for (let i = 0; i < currencies.length - 1; i++) {
 			const currency = currencies[i];
-			const otherCurrencies = currencies.filter((_, ii) => ii > i);
 
-			const response = await fetch(`http://api.exchangerate.host/live?access_key=${process.env.exchangerate_key}&source=${currency}&currencies=${otherCurrencies.join(",")}`)
+			const response = await fetch(`http://api.exchangerate.host/live?access_key=${process.env.exchangerate_key}&source=${currency}&currencies=${currencies.join(",")}`)
 				.then(res => res.json());
+			
+			sublogger.trace(response);
+
 			const Z = new Zod(z.object({
 				success: z.literal(true),
-				// eslint-disable-next-line @typescript-eslint/no-unused-vars
-				quotes: z.object(arr2obj(otherCurrencies.map(oc => currency + oc), otherCurrencies.map(_ => z.number())))
+				source: z.literal(currency),
+				quotes: z.object(arr2obj(currencies.map(c => currency + c), currencies.map(_ => z.number())))
 			}));
 			assert(Z.check(response));
 
-			for (const _currency of otherCurrencies) {
-				await prisma.currencyRecord.create({
-					data: {
-						src: currency,
-						dst: _currency,
-						value: response.quotes[currency + _currency],
-						updateInfo: {
-							connect: {
-								id: updateInfo.id
-							}
-						}
-					}
-				});
-			}
+			quotes = {
+				...quotes,
+				...response.quotes
+			};
 		}
 
 		return true;
 	} catch (e) {
+		sublogger.error(e);
 		return false;
 	}
 }
 
 worker();
-setInterval(worker, 3600 * 1000);
-
+setInterval(worker, 86400 * 2 * 1000);
 
 class Command extends SlashApplicationCommand {
 	public options: LApplicationCommandOptionData[] = [
@@ -101,54 +126,29 @@ class Command extends SlashApplicationCommand {
 		const amount = interaction.options.getNumber("amount") ?? 1;
 		const target = interaction.options.getString("target") ?? null;
 
-		const records = await prisma.currencyRecord.findMany({
-			where: {
-				OR: [
-					{
-						src: source
-					}, {
-						dst: source
-					}
-				]
-			},
-			orderBy: {
-				updateInfo: {
-					time: "desc"
-				}
-			},
-			include: {
-				updateInfo: true
-			},
-			take: currencies.length - 1
-		});
-
-		assert(records.length === currencies.length - 1);
-
 		return {
 			embeds: [{
 				title: "Convert",
 				fields: [
 					{
-						name: source,
+						name: {
+							key: `currency._${source}_name`
+						},
 						value: amount.toString()
 					},
-					...records
-						.filter(record => target === null ? true : record.dst === target || record.src === target)
-						.map(record => {
-							// Assumption: src -> dst = 1 / dst -> src
-
-							const inverseFlag = record.dst === source;
-							return {
-								name: inverseFlag ? record.src : record.dst,
-								value: `${round((inverseFlag ? 1 / record.value : record.value) * amount, 3)}`,
-								inline: true
-							}
-						})
+					{
+						name: target ? {
+							key: `currency._${target}_name`
+						} : "All",
+						value: target ? 
+							getQuote(source, target, amount).toString() : 
+							currencies.map(currency => `${currency}: ${getQuote(source, currency, amount)}`).join("\n")
+					}
 				],
 				footer: {
 					text: "Updated at"
 				},
-				timestamp: records[0].updateInfo.time.toISOString()
+				timestamp: lastUpdated.toISOString()
 			}]
 		};
 	}
